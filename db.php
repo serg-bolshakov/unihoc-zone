99297e9e7365d7809487356fa0c9d0da505cc0fd
serg-bolshakov <you@example.com>
2023-05-18 20:53:23 +0300

    deleted comments

--- db.php
+++ db.php
@@ -11,16 +11,3 @@ if ($connect->errno) {
     echo $connect->error;
     die();
 }
-
-
-// $host = 'localhost';
-// $username = 'u127283_serg.bolshakov';
-// $password = 'bazadannykh%1994';
-// $db = 'u127283_floorball_db';
-
-// $connect = new mysqli($host, $username, $password, $db);
-
-// if ($connect->errno) {
-//     echo $connect->error;
-//     die();
-// }
