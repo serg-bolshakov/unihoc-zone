db4c0cc0c93399c48acd7b145d4aebaed2015cd0
serg-bolshakov <you@example.com>
2023-06-26 22:44:05 +0300

    comments were added

--- elems/pagination.php
+++ elems/pagination.php
@@ -2,21 +2,25 @@
 <?php
         $params = $_GET; // в переменную $params кладётся массив $_GET - Ассоциативный массив параметров, переданных скрипту через URL.
         // array(1) { ["page"]=> string(1) "3" } (когда выбрали для просмотра третью страницу)
-
+        // array(1) { ["brand"]=> array(1) { [0]=> string(14) "ZONEFLOORBALL." } } - когда выбрали в фильтре бренд...
+            
         unset($params['page']); // результат: array(0) { } 
         // unset() удаляет перечисленные переменные.
         // Если переменная, объявленная глобальной, удаляется внутри функции, то будет удалена только локальная переменная. 
         //Переменная в области видимости вызова функции сохранит то же значение, что и до вызова unset().
         // удаляем один элемент массива: unset($bar['quux']);
 
-        $query = http_build_query($params); // передаём: array(1) { ["page"]=> string(1) "3" } 
-                                            // получаем в результате: string(6) "page=3"
-        
-        
+        // var_dump($params); - array(2) { ["shaft_flex"]=> array(4) { [0]=> string(2) "36" [1]=> string(2) "30" [2]=> string(2) "29" 
+        // [3]=> string(2) "27" } ["brand"]=> array(1) { [0]=> string(6) "UNIHOC" } }
+
+        $query = http_build_query($params); //http_build_query - Генерирует URL-кодированную строку запроса
+        //var_dump($query); string(27) "brand%5B0%5D=ZONEFLOORBALL."
+        //var_dump($query); - string(103) "shaft_flex%5B0%5D=36&shaft_flex%5B1%5D=30&shaft_flex%5B2%5D=29&shaft_flex%5B3%5D=27&brand%5B0%5D=UNIHOC"   
+
         // проверяем для стрелочек влево, что мы на первой странице ... если на первой стрелки деактивируются, но остаются видимыми
         if ($page != 1){
             $prev = $page - 1; //предыдущая страница
-            echo "<a href=\"?page=$prev&{$query}\"><< </a> "; // я до этого делал без {&$query}
+            echo "<a href=\"?page=$prev&{$query}\">&lt;&lt; </a> "; // я до этого делал без {&$query}
         } else {
             echo "&lt;&lt; ";
         }
@@ -30,7 +34,7 @@
                 
         if ($page != $pagesCount) {
             $next = $page + 1; //следующая страница
-            echo "<a href=\"?page=$next&{$query}\">>></a> ";
+            echo "<a href=\"?page=$next&{$query}\"> &gt;&gt;</a> ";
         } else {
             echo " &gt;&gt;";
         }         
